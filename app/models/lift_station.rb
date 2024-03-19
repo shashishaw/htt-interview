2f0da1c04123a25032b050db109a2cda218e09f0
Parker Hodge <parker.hodge@htt.io>
2024-03-19 12:54:26 -0500

    add comment

--- app/models/lift_station.rb
+++ app/models/lift_station.rb
@@ -6,11 +6,13 @@
 class LiftStation < ApplicationRecord
   belongs_to :pump, class_name: 'Pump'
 
+  # TODO: implement method
   # The total volume of the lift stations tank
   def total_tank_volume
     raise NotImplementedError, 'This method still needs to be implemented.'
   end
 
+  # TODO: implement method
   # The volume of the tank from the height of the off float to the height of the lead float
   def lead_to_off_volume
     raise NotImplementedError, 'This method still needs to be implemented.'
