cce98d0a81781702a8c8ba5de8d1d4e8c3e6717d
Shashikant <shashishaw030.com>
2024-03-26 01:00:13 +0530

    Add lift stations method

--- app/models/lift_station.rb
+++ app/models/lift_station.rb
@@ -8,13 +8,30 @@ class LiftStation < ApplicationRecord
 
   # TODO: implement method
   # The total volume of the lift stations tank
+
+  # Method to calculate the total volume of the lift station's tank
   def total_tank_volume
-    raise NotImplementedError, 'This method still needs to be implemented.'
+    Math::PI * radius**2 * height
   end
 
-  # TODO: implement method
-  # The volume of the tank from the height of the off float to the height of the lead float
+  # Method to calculate the volume of the tank from the height of the off float to the height of the lead float
   def lead_to_off_volume
-    raise NotImplementedError, 'This method still needs to be implemented.'
+    Math::PI * radius**2 * (lead_to_floor - off_to_floor)
+  end
+
+  def inflow_rate
+    inflow_volume = lead_to_off_volume
+    inflow_time = pump_cycles.sum(:duration)
+    inflow_volume / inflow_time if inflow_time > 0
+  end
+
+  def outflow_rate
+    outflow_volume = lead_to_off_volume
+    outflow_time = pump_cycles.sum(:duration)
+    outflow_volume / outflow_time if outflow_time > 0
+  end
+
+  def flow_total
+    lead_to_off_volume * pump_cycles.count
   end
 end
